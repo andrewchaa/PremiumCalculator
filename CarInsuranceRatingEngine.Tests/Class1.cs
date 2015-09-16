b3a92550798ff0f3204a4aa4b4d6ebec126f3a95
Andrew <andrew.chaa@yahoo.co.uk>
2015-09-16 23:36:55 +0100

    initial load

---
+++ CarInsuranceRatingEngine.Tests/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CarInsuranceRatingEngine.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void It_should_calculate_premium()
+        {
+            var calculator = new RateCalculator();
+            var premium = calculator.CalculateRate("Car", "Audi");
+            Assert.That(premium, Is.EqualTo(1200));
+        }
+    }
+
+    public class RateCalculator
+    {
+        public int CalculateRate(string vehicleType, string manufacturer)
+        {
+            return 1200;
+        }
+    }
+}
