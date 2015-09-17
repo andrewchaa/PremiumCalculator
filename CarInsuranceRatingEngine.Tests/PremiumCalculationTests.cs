a3073911d5946772e126fbc89a1d4728897290eb
andrewchaa <andrew.chaa@huddle.com>
2015-09-17 13:30:25 +0100

    premium test cases

--- CarInsuranceRatingEngine.Tests/PremiumCalculationTests.cs
+++ CarInsuranceRatingEngine.Tests/PremiumCalculationTests.cs
@@ -7,15 +7,48 @@ namespace CarInsuranceRatingEngine.Tests
     [TestFixture]
     public class PremiumCalculationTests
     {
+        private PremiumCalculator _premiumCalculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _premiumCalculator = new PremiumCalculator(new BasePremiumStore(), new ManufacturerFactorStore());
+        }
+
         [Test]
         public void It_should_calculate_premium_for_Audi_Car()
         {
-            var premiumCalculator = new PremiumCalculator(new BasePremiumStore(), new ManufacturerFactorStore());
-
-            var audi = new Car(new Audi());
-            var premium = premiumCalculator.Calculate(audi);
+            var audiCar = new Car(new Audi());
+            var premium = _premiumCalculator.Calculate(audiCar);
 
             Assert.That(premium, Is.EqualTo(1200));
+        }
+
+        [Test]
+        public void It_should_calculate_premium_for_Audi_Van()
+        {
+            var audiVan = new Van(new Audi());
+            var premium = _premiumCalculator.Calculate(audiVan);
+
+            Assert.That(premium, Is.EqualTo(1500));
+        }
+
+        [Test]
+        public void It_should_calculate_premium_for_Mercedes_Car()
+        {
+            var mercedesCar = new Car(new Mercedes());
+            var premium = _premiumCalculator.Calculate(mercedesCar);
+
+            Assert.That(premium, Is.EqualTo(1600));
+        }
+
+        [Test]
+        public void It_should_calculate_premium_for_Mercedes_Van()
+        {
+            var mercedesVan = new Van(new Mercedes());
+            var premium = _premiumCalculator.Calculate(mercedesVan);
+
+            Assert.That(premium, Is.EqualTo(2000));
         }
     }
 }
